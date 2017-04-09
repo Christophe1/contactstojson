5afaa302f12b5249042e475ebcba4dc782569a1f
Christophe <harris.christophe@gmail.com>
2017-04-10 00:15:13 +0100

    working on Volley

--- app/src/main/java/com/example/chris/contactstojson/MainActivity.java
+++ app/src/main/java/com/example/chris/contactstojson/MainActivity.java
@@ -13,6 +13,13 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
@@ -21,14 +28,28 @@ import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
 
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // this is the php file name where to select from the database, the user's phone number
+    private static final String CHECKPHONENUMBER_URL = "http://www.populisto.com/check_if_contact_is_a_user.php";
+
+    //we are posting phoneNo, which in PHP is phonenumber
+    public static final String KEY_PHONENUMBER = "phonenumber";
+
+    Button buttonCheck;
+    String phoneNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonCheck = (Button) findViewById(R.id.buttonCheck);
+        buttonCheck.setOnClickListener(this);
 
         //get the names and phone numbers of all contacts in phone book
         ContentResolver cr = getContentResolver();
@@ -47,7 +68,7 @@ public class MainActivity extends Activity {
                     Cursor pCur = cr.query(
                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                             null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{id}, null);
                     while (pCur.moveToNext()) {
                         String phoneNo = pCur.getString(pCur.getColumnIndex(
@@ -83,30 +104,56 @@ public class MainActivity extends Activity {
                 }
             }
         }
+    }
 
-        //I need to check if a contact in the user's phone contacts is already a user of populisto.
-        //If yes, then in the contacts table put in the user's user_id
-        //and the contact's user id in contacts_id
+    //I need to check if a contact in the user's phone contacts is already a user of populisto.
+    //If yes, then in the contacts table put in the user's user_id
+    //and the contact's user id in contacts_id
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
 
+    @Override
+    public void onClick(View v) {
+        phoneNo = "+353864677745";
+        if (v == buttonCheck) {
+            CheckifUserisContact();
+        }
+    }
+
+
+    private void CheckifUserisContact() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, CHECKPHONENUMBER_URL,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.equals("success")) {
+                            Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }) {
             @Override
-            public void onClick(View v) {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(KEY_PHONENUMBER, phoneNo);
+                return params;
+            }
 
-                String numberStr = "4935837";
-                PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
-                try {
-                    Phonenumber.PhoneNumber numberProto = phoneUtil.parse(numberStr, "IE");
-                    //Since you know the country you can format it as follows:
-                    System.out.println(phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.E164));
-                } catch (NumberParseException e) {
-                    System.err.println("NumberParseException was thrown: " + e.toString());
-                }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
-                }
-
-
-    });
+    }
 }
-}
+
